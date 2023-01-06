38dd074f77142e1f47e5e6e4265a141ca75ec248
Jeff McMillan <10823453+jeffmc@users.noreply.github.com>
2023-01-06 13:31:31 -0800

    new file:   main.cpp 	modified:   node.hpp 	new file:   student.h

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// This is a barebones program, with no real functionality yet, 
+// but only the linked list data structure implemented.
+
+#include <iostream>
+#include <cstring>
+
+#include "node.hpp"
+
+// This is the entry point of the program.
+int main() {
+	Node* head = new Node(new Student);
+	
+	printf("Welcome to Linked Lists!\n");
+
+}
---
+++ student.h
@@ -0,0 +1,7 @@
+// This class contains all student information, the functionality has not yet been implemented.
+class Student {
+	char first[32];
+	char last[32];
+	int id;
+	float gpa;
+};
